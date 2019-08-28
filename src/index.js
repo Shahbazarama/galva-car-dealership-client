465a381138753bf628f8e265a76016f4416dc24b
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-08-28 10:38:08 -0700

    set up react router

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
